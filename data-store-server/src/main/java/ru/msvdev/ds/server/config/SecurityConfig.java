435e7d7b84186fe05dd2dd5b0689db28079e1c40
msvdev-k <msvdev.k@gmail.com>
2024-08-07 19:05:52 +0300

    Настроено разграничение доступа (полномочия) пользователей при работе с картотекой

--- data-store-server/src/main/java/ru/msvdev/ds/server/config/SecurityConfig.java
+++ data-store-server/src/main/java/ru/msvdev/ds/server/config/SecurityConfig.java
@@ -2,11 +2,11 @@ package ru.msvdev.ds.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import ru.msvdev.ds.server.security.AccessService;
 import ru.msvdev.ds.server.security.access.CatalogBasedAccessService;
 import ru.msvdev.ds.server.security.AuthorityService;
 
+import static org.springframework.http.HttpMethod.*;
 import static ru.msvdev.ds.server.security.Authority.*;
 
 
@@ -18,31 +18,31 @@ public class SecurityConfig {
         return CatalogBasedAccessService.builder()
                 .setAuthorityService(authorityService)
 
-                .addMatcher("catalog", HttpMethod.GET)
-                .addMatcher("catalog", HttpMethod.POST)
-                .addMatcher("catalog/{catalogId}", HttpMethod.PUT, MASTER)
-                .addMatcher("catalog/{catalogId}", HttpMethod.DELETE, MASTER)
-                .addMatcher("catalog/{catalogId}/field", HttpMethod.GET, MASTER, READING)
-                .addMatcher("catalog/{catalogId}/field", HttpMethod.POST, MASTER, FIELD_TEMPLATE_WRITING)
-                .addMatcher("catalog/{catalogId}/field/*", HttpMethod.PUT, MASTER, FIELD_TEMPLATE_WRITING)
-                .addMatcher("catalog/{catalogId}/field/*", HttpMethod.DELETE, MASTER, FIELD_TEMPLATE_DELETING)
-                .addMatcher("catalog/{catalogId}/card", HttpMethod.GET, MASTER, READING)
-                .addMatcher("catalog/{catalogId}/card", HttpMethod.POST, MASTER, WRITING)
-                .addMatcher("catalog/{catalogId}/card/*", HttpMethod.GET, MASTER, READING)
-                .addMatcher("catalog/{catalogId}/card/*", HttpMethod.DELETE, MASTER, DELETING)
-                .addMatcher("catalog/{catalogId}/card/*/tag", HttpMethod.PUT, MASTER, WRITING)
-                .addMatcher("catalog/{catalogId}/card/*/tag", HttpMethod.DELETE, MASTER, WRITING)
-                .addMatcher("catalog/{catalogId}/card/*/tag/*", HttpMethod.DELETE, MASTER, WRITING)
-                .addMatcher("catalog/{catalogId}/user", HttpMethod.GET, MASTER, GRANT_AUTHORITY)
-                .addMatcher("catalog/{catalogId}/user", HttpMethod.PUT, MASTER, GRANT_AUTHORITY)
-                .addMatcher("catalog/{catalogId}/user", HttpMethod.DELETE, MASTER, GRANT_AUTHORITY)
-                .addMatcher("catalog/{catalogId}/upload", HttpMethod.POST, MASTER, FILE_UPLOAD)
-                .addMatcher("catalog/{catalogId}/upload", HttpMethod.PUT, MASTER, FILE_UPLOAD)
-                .addMatcher("catalog/{catalogId}/fs/*", HttpMethod.GET, MASTER)
-                .addMatcher("catalog/{catalogId}/fs/*", HttpMethod.POST, MASTER)
-                .addMatcher("catalog/{catalogId}/fs/*", HttpMethod.PUT, MASTER)
-                .addMatcher("catalog/{catalogId}/fs/*", HttpMethod.DELETE, MASTER)
-                .addMatcher("catalog/{catalogId}/fs/*/download", HttpMethod.GET, MASTER)
+                .addMatcher("catalog", GET)
+                .addMatcher("catalog", POST)
+                .addMatcher("catalog/{catalogId}", PUT, MASTER)
+                .addMatcher("catalog/{catalogId}", DELETE, MASTER)
+                .addMatcher("catalog/{catalogId}/field", GET, MASTER, READING, FIELD_TEMPLATE_WRITING)
+                .addMatcher("catalog/{catalogId}/field", POST, MASTER, FIELD_TEMPLATE_WRITING)
+                .addMatcher("catalog/{catalogId}/field/*", PUT, MASTER, FIELD_TEMPLATE_WRITING)
+                .addMatcher("catalog/{catalogId}/field/*", DELETE, MASTER, FIELD_TEMPLATE_DELETING)
+                .addMatcher("catalog/{catalogId}/card", GET, MASTER, READING)
+                .addMatcher("catalog/{catalogId}/card", POST, MASTER, WRITING)
+                .addMatcher("catalog/{catalogId}/card/*", GET, MASTER, READING)
+                .addMatcher("catalog/{catalogId}/card/*", DELETE, MASTER, DELETING)
+                .addMatcher("catalog/{catalogId}/card/*/tag", PUT, MASTER, WRITING)
+                .addMatcher("catalog/{catalogId}/card/*/tag", DELETE, MASTER, DELETING)
+                .addMatcher("catalog/{catalogId}/card/*/tag/*", DELETE, MASTER, WRITING)
+                .addMatcher("catalog/{catalogId}/user", GET, MASTER, GRANT_AUTHORITY)
+                .addMatcher("catalog/{catalogId}/user", PUT, MASTER, GRANT_AUTHORITY)
+                .addMatcher("catalog/{catalogId}/user", DELETE, MASTER, GRANT_AUTHORITY)
+                .addMatcher("catalog/{catalogId}/upload", POST, MASTER, FILE_UPLOAD)
+                .addMatcher("catalog/{catalogId}/upload", PUT, MASTER, FILE_UPLOAD)
+                .addMatcher("catalog/{catalogId}/fs/*", GET, MASTER, FILE_SYSTEM_READ)
+                .addMatcher("catalog/{catalogId}/fs/*", POST, MASTER, FILE_SYSTEM_WRITE)
+                .addMatcher("catalog/{catalogId}/fs/*", PUT, MASTER, FILE_SYSTEM_WRITE)
+                .addMatcher("catalog/{catalogId}/fs/*", DELETE, MASTER, FILE_SYSTEM_DELETE)
+                .addMatcher("catalog/{catalogId}/fs/*/download", GET, MASTER, FILE_DOWNLOAD)
 
                 .build();
     }
